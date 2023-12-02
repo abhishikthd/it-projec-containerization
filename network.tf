cf7c855665213182a7a2e2b8af4a99ee8b78401d
abhishikthd <abhishikth02@gmail.com>
2023-12-01 23:37:07 -0500

    Creating AWS resource v3

--- network.tf
+++ network.tf
@@ -17,6 +17,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "subnet_a" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
