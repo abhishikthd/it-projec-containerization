58a119249b2ca81db997b928153c9ac8a99fb8d5
abhishikthd <abhishikth02@gmail.com>
2023-12-01 23:38:03 -0500

    Creating AWS resource v3

--- network.tf
+++ network.tf
@@ -7,14 +7,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
+  region = "us-east-1"  # Chhange to your desired AWS region
 }
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet_a" {
+resource "aws_subnet" "subnet_a" { 
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1b"
